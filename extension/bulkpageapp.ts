7bf22a6bb71069c653e79e85890cf03ce88a8fde
HuskerSam <sam.huelsdonk@gmail.com>
2024-04-14 20:18:31 -0500

    tabulator in bulk page

--- extension/bulkpageapp.ts
+++ extension/bulkpageapp.ts
@@ -1,4 +1,5 @@
 import { AnalyzerExtensionCommon } from './extensioncommon';
+import { TabulatorFull } from 'tabulator-tables';
 import SlimSelect from 'slim-select';
 declare const chrome: any;
 
@@ -7,8 +8,30 @@ export default class BulkPageApp {
     extCommon = new AnalyzerExtensionCommon(chrome);
     bulk_analysis_sets_select: any = document.querySelector('.bulk_analysis_sets_select');
     bulkSelected: SlimSelect;
+    bulk_url_list_tabulator: TabulatorFull;
+    add_bulk_url_row = document.querySelector('.add_bulk_url_row') as HTMLButtonElement;
 
     constructor() {
+        this.bulk_url_list_tabulator = new TabulatorFull(".bulk_url_list_tabulator", {
+          layout: "fitColumns",
+          columns: [
+            { title: "URL", field: "url", width: 500, editor:"input" },
+            { title: "Options", field: "analysis_set", width: 200 },
+          ],
+        });
+        this.bulk_url_list_tabulator.on("cellEdited", async (cell: any) => {
+          let bulkUrlList = this.bulk_url_list_tabulator.getData();
+              await chrome.storage.local.set({ bulkUrlList });
+      });
+        this.add_bulk_url_row.addEventListener('click', async () => {
+          let bulkUrlList = this.bulk_url_list_tabulator.getData();
+          bulkUrlList.push({ url: "", analysis_set: "" });
+         this.bulk_url_list_tabulator.setData(bulkUrlList);
+          await chrome.storage.local.set({ bulkUrlList });
+        });
+
+        
+
         this.bulkSelected = new SlimSelect({
           select: '.bulk_analysis_sets_select',
           settings: {
@@ -36,10 +59,24 @@ export default class BulkPageApp {
           },
         });
 
-        this.paint();
+        
+        chrome.storage.local.onChanged.addListener(() => {
+          this.paintData();
+      });
+      this.paintData();
     }
 
-    async paint() {
+    
+
+    async paintData() {
+      let allUrls: any[] = []; 
+        let rawData = await chrome.storage.local.get('bulkUrlList');
+        if (rawData && rawData.bulkUrlList && Object.keys(rawData.bulkUrlList).length > 0) {
+          allUrls = rawData.bulkUrlList;
+        }
+      
+      this.bulk_url_list_tabulator.setData(allUrls);
+
         let running = await chrome.storage.local.get('running');
         if (running && running.running) {
           document.body.classList.add("extension_running");
