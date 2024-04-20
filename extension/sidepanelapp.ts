f1d97be43d6f66a13019f555181adffa2343cfd1
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2024-04-20 13:05:25 -0500

    toggle side panel functional

--- extension/sidepanelapp.ts
+++ extension/sidepanelapp.ts
@@ -1,22 +1,30 @@
 import { AnalyzerExtensionCommon } from './extensioncommon';
 declare const chrome: any;
 
-
 export default class SidePanelApp {
     extCommon = new AnalyzerExtensionCommon(chrome);
     show_main_page_btn = document.querySelector('.show_main_page_btn') as HTMLButtonElement;
+    lastPanelToggleDate = new Date().toISOString();
 
     constructor() {
         this.extCommon.initCommonDom();
         chrome.storage.local.onChanged.addListener(() => {
-            this.extCommon.paintAnalysisTab();
+            this.handleStorageChange();
         });
-        this.extCommon.paintAnalysisTab();
-        
-        if (this.show_main_page_btn) {
-            this.show_main_page_btn.addEventListener('click', () => {
-                chrome.tabs.create({ url: 'main.html', active: true });
-            });
+        this.handleStorageChange();
+
+        this.show_main_page_btn.addEventListener('click', () => {
+            chrome.tabs.create({ url: 'main.html', active: true });
+        });
+    }
+    async handleStorageChange() {
+        let lastPanelToggleDate = await chrome.storage.local.get('lastPanelToggleDate');
+        if (lastPanelToggleDate && lastPanelToggleDate.lastPanelToggleDate) lastPanelToggleDate = lastPanelToggleDate.lastPanelToggleDate;
+
+        if (lastPanelToggleDate > this.lastPanelToggleDate) {
+            window.close();
+            return;
         }
+        this.extCommon.paintAnalysisTab();
     }
 }
