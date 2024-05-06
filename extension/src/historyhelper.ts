3ae2489cccd702688b96e19420e6a5306144ee8b
HuskerSam <sam.huelsdonk@gmail.com>
2024-05-06 15:40:10 -0500

    onboarding visuals

--- extension/src/historyhelper.ts
+++ extension/src/historyhelper.ts
@@ -60,11 +60,20 @@ export default class HistoryHelper {
         let usageCreditTotal = 0;
         let entry = history[this.baseHistoryIndex];
         let entryHTML = `
-        <div class="history_empty">
-            <p class="history_empty_onboarding_message">
-                No history found. <br><br>
-                Select metric and provide text input to begin.
-            </p>
+        <div class="block">
+            <div class="rounded-md bg-blue-50 p-4 history_empty w-full h-auto">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <svg class="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3 flex-1 md:flex md:justify-between">
+                        <p class="text-sm text-blue-700">No history found.<br><br>
+                           Use sidebar to select a metric, input text and run an analysis to begin.</p>
+                    </div>
+                </div>
+            </div>
         </div>
         `;
         if (entry) {
@@ -79,7 +88,7 @@ export default class HistoryHelper {
             this.entry_total_credit_usage.innerHTML = `Credits: ${Math.round(usageCreditTotal)}`;
             this.history_date.innerHTML = this.extCommon.showGmailStyleDate(entry.runDate);
         } else {
-            this.history_text.innerHTML = '';
+            this.history_text.innerHTML = 'Analysis text for each entry will be displayed here...';
             this.entry_total_credit_usage.innerHTML = '';
             this.history_date.innerHTML = '';
             this.url_display.innerHTML = '';
